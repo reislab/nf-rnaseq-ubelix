fac986e2d889548f5792051cd36c7ad4095116f5
rodrigoreis <rodrigoreis1@gmail.com>
2023-04-09 18:43:46 -0300

    first script fixed-1

--- nf-rnaseq-ubelix-01.sh
+++ nf-rnaseq-ubelix-01.sh
@@ -3,11 +3,16 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=1GB
 
+# Set working directory to your project folder (e.g., <reis> below)
+$WORKDIR=/storage/workspaces/ips_reislab/reislab/reis
 
-mkdir -p ~/nf-rnaseq
+# Clone git repo
+git clone https://github.com/reislab/nf-rnaseq-ubelix.git
 
-$OUTDIR=~/nf-rnaseq/results
+# Setup directory structure
+mkdir -p $WORKDIR/nf-rnaseq-ubelix/results
+cd $WORKDIR
+$OUTDIR=/nf-rnaseq/results
 
-cd ~/nf-rnaseq
-
+# Run pipeline
 nextflow run nf-core/rnaseq -r 3.11.1 -profile test --outdir $OUTDIR
