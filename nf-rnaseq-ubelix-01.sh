5683558738042fbe876ecc80fb608e6495d94f7a
rodrigoreis <rodrigoreis1@gmail.com>
2023-04-09 22:29:39 -0300

    fix

--- nf-rnaseq-ubelix-01.sh
+++ nf-rnaseq-ubelix-01.sh
@@ -6,13 +6,10 @@
 module load Workspace
 module load Nextflow/22.10.1
 
-# Set working directory to your project folder (e.g., <reis> below)
-$WORKDIR=/storage/workspaces/ips_reislab/reislab/reis
-
 # Setup directory structure
-mkdir -p $WORKDIR/nf-rnaseq-ubelix/results
-cd $WORKDIR
-$OUTDIR=/nf-rnaseq/results
+mkdir -p $WORKSPACE/reis/nf-rnaseq-ubelix/results
+$OUTDIR="$WORKSPACE/reis/nf-rnaseq/results"
+cd $WORKSPACE/reis/nf-rnaseq-ubelix
 
 # Run pipeline
 nextflow run nf-core/rnaseq -r 3.11.1 -profile test --outdir $OUTDIR
